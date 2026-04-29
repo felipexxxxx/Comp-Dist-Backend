49dffc56f1ebb7977fe6ca5ce47b794b7f222816
Felipe Lins <felipeplinsesilva@gmail.com>
2026-04-29 12:29:26 -0300

    fix(infra): initialize service databases idempotently

--- infra/postgres/init/01-create-databases.sql
+++ infra/postgres/init/01-create-databases.sql
@@ -1,4 +1,11 @@
-CREATE DATABASE healthsys_identity;
-CREATE DATABASE healthsys_patient;
-CREATE DATABASE healthsys_triage;
-CREATE DATABASE healthsys_notification;
+SELECT 'CREATE DATABASE healthsys_identity'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'healthsys_identity')\gexec
+
+SELECT 'CREATE DATABASE healthsys_patient'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'healthsys_patient')\gexec
+
+SELECT 'CREATE DATABASE healthsys_triage'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'healthsys_triage')\gexec
+
+SELECT 'CREATE DATABASE healthsys_notification'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'healthsys_notification')\gexec
